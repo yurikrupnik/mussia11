9aa5ac52b6717a33337ad0dc418969b4791d86db
Tal Efronny <talefronny@gmail.com>
2021-09-06 12:19:38 +0300

    fixed e2e

--- apps/internal/jarvis-e2e/src/integration/app.spec.ts
+++ apps/internal/jarvis-e2e/src/integration/app.spec.ts
@@ -8,6 +8,6 @@ describe('internal-jarvis', () => {
     cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to internal-jarvis!');
+    getGreeting().contains('Welcome to jarvis!');
   });
 });
